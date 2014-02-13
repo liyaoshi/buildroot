cdc59cdec15a918591425f488f427da3fff052fd
liyaoshi <liyaoshi@gmail.com>
2014-02-13 09:52:07 +0800

    update freescale package 2 3.10.17

--- package/freescale-imx/freescale-imx.mk
+++ package/freescale-imx/freescale-imx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FREESCALE_IMX_VERSION = 3.5.7-1.0.0
+FREESCALE_IMX_VERSION = 3.10.17-1.0.0_beta
 
 # No official download site from freescale, just this mirror
 FREESCALE_IMX_MIRROR_SITE   = http://download.ossystems.com.br/bsp/freescale/source
