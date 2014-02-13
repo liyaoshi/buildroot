b75e641cf45b4a47814af4071713a1a861ef1e8a
liyaoshi <liyaoshi@gmail.com>
2014-02-13 15:11:42 +0800

    update wayland and weston to 1.3.93

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WESTON_VERSION = 1.3.0
+WESTON_VERSION = 1.3.93
 WESTON_SITE = http://wayland.freedesktop.org/releases/
 WESTON_SOURCE = weston-$(WESTON_VERSION).tar.xz
 WESTON_LICENSE = MIT
