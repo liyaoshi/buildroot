0cc964c1e8f3cedcd833afab46dedf5b198a4c23
liyaoshi <liyaoshi@gmail.com>
2014-02-13 10:39:51 +0800

    work around for imx gpu package build

--- package/freescale-imx/freescale-imx.mk
+++ package/freescale-imx/freescale-imx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FREESCALE_IMX_VERSION = 3.10.17-1.0.0_beta
+FREESCALE_IMX_VERSION = 3.10.17-1.0.0
 
 # No official download site from freescale, just this mirror
 FREESCALE_IMX_MIRROR_SITE   = http://download.ossystems.com.br/bsp/freescale/source
