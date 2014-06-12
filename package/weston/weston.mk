279f54b9ee5bc66af2102d8a789e83fec43925b9
liyaoshi <liyaoshi@gmail.com>
2014-06-12 16:11:02 +0800

    update weston cairo patch

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -31,12 +31,12 @@ WESTON_CONF_OPT = \
 
 ifeq ($(BR2_PACKAGE_WESTON_FBDEV),y)
 WESTON_CONF_OPT += --enable-fbdev-compositor \
-           COMPOSITOR_CFLAGS="-I$${SYSROOT}/usr/include/pixman-1 -DLINUX=1 -DEGL_API_FB " \
+           COMPOSITOR_CFLAGS="-I$${SYSROOT}/usr/include/pixman-1 -I$${SYSROOT}/usr/include/cairo -DLINUX=1 -DEGL_API_FB " \
            COMPOSITOR_LIBS="-lGLESv2 -lEGL -lGAL -lwayland-server -lxkbcommon -lpixman-1"  \
            LDFLAGS="-lwayland-server  -lEGL  -lwayland-cursor -lpixman-1"  \
-           CLIENT_CFLAGS="-DLINUX=1 -DEGL_API_FB"  \
+           CLIENT_CFLAGS="-I$${SYSROOT}/usr/include/cairo -DLINUX=1 -DEGL_API_FB"  \
            CLIENT_LIBS="-lGLESv2 -lEGL -lwayland-client -lEGL -lwayland-cursor -lxkbcommon -lcairo" \
-           SIMPLE_EGL_CLIENT_CFLAGS="-DLINUX=1 -DEGL_API_FB -DEGL_API_WL " \
+           SIMPLE_EGL_CLIENT_CFLAGS="-I$${SYSROOT}/usr/include/cairo -DLINUX=1 -DEGL_API_FB -DEGL_API_WL " \
            SIMPLE_EGL_CLIENT_LIBS="-lGLESv2 -lEGL -lwayland-client -lwayland-cursor -lcairo" \
 	   SIMPLE_CLIENT_LIBS="-lGLESv2 -lEGL  -lwayland-client -lwayland-cursor -lcairo" \
            IMAGE_LIBS="-lwayland-cursor -lcario" \
