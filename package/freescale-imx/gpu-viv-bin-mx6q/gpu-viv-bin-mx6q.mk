0465a031bb6766ad1274d67d8bc586b1f22133df
liyaoshi <liyaoshi@gmail.com>
2014-06-13 16:13:27 +0800

    update gpu package , use from fsl site

--- package/freescale-imx/gpu-viv-bin-mx6q/gpu-viv-bin-mx6q.mk
+++ package/freescale-imx/gpu-viv-bin-mx6q/gpu-viv-bin-mx6q.mk
@@ -6,7 +6,7 @@
 
 GPU_VIV_BIN_MX6Q_VERSION = $(FREESCALE_IMX_VERSION)
 GPU_VIV_BIN_MX6Q_SITE    = $(FREESCALE_IMX_MIRROR_SITE)
-GPU_VIV_BIN_MX6Q_SOURCE  = gpu-viv-bin-mx6q-3.10.17-1.0.0-beta-hfp.bin
+GPU_VIV_BIN_MX6Q_SOURCE  = gpu-viv-bin-mx6q-3.10.17-1.0.0-hfp.bin
 
 GPU_VIV_BIN_MX6Q_INSTALL_STAGING = YES
 
@@ -41,24 +41,24 @@ endef
 # in the upstream archive here.
 # Make sure these commands are idempotent.
 define GPU_VIV_BIN_MX6Q_BUILD_CMDS
-	$(SED) 's/defined(LINUX)/defined(__linux__)/g' $(@D)-beta-hfp/usr/include/*/*.h
+	$(SED) 's/defined(LINUX)/defined(__linux__)/g' $(@D)-hfp/usr/include/*/*.h
 
-	ln -sf libEGL-wl.so $(@D)-beta-hfp/usr/lib/libEGL.so
-	ln -sf libEGL-wl.so $(@D)-beta-hfp/usr/lib/libEGL.so.1
+	ln -sf libEGL-wl.so $(@D)-hfp/usr/lib/libEGL.so
+	ln -sf libEGL-wl.so $(@D)-hfp/usr/lib/libEGL.so.1
 
-	ln -sf libGAL-wl.so $(@D)-beta-hfp/usr/lib/libGAL.so
+	ln -sf libGAL-wl.so $(@D)-hfp/usr/lib/libGAL.so
 
-	ln -sf libGLESv2-wl.so $(@D)-beta-hfp/usr/lib/libGLESv2.so
-	ln -sf libGLESv2-wl.so $(@D)-beta-hfp/usr/lib/libGLESv2.so.2
+	ln -sf libGLESv2-wl.so $(@D)-hfp/usr/lib/libGLESv2.so
+	ln -sf libGLESv2-wl.so $(@D)-hfp/usr/lib/libGLESv2.so.2
 
-	ln -sf libVIVANTE-wl.so $(@D)-beta-hfp/usr/lib/libVIVANTE.so
+	ln -sf libVIVANTE-wl.so $(@D)-hfp/usr/lib/libVIVANTE.so
 
-	ln -sf libGL.so.1.2 $(@D)-beta-hfp/usr/lib/libGL.so.1
-	ln -sf libGL.so.1.2 $(@D)-beta-hfp/usr/lib/libGL.so
+	ln -sf libGL.so.1.2 $(@D)-hfp/usr/lib/libGL.so.1
+	ln -sf libGL.so.1.2 $(@D)-hfp/usr/lib/libGL.so
 endef
 
 define GPU_VIV_BIN_MX6Q_INSTALL_STAGING_CMDS
-	cp -r $(@D)-beta-hfp/usr/* $(STAGING_DIR)/usr
+	cp -r $(@D)-hfp/usr/* $(STAGING_DIR)/usr
 	for lib in egl glesv2 vg; do \
 		$(INSTALL) -m 0644 -D \
 			package/freescale-imx/gpu-viv-bin-mx6q/$${lib}.pc \
@@ -73,7 +73,7 @@ endef
 ifeq ($(BR2_PACKAGE_GPU_VIV_BIN_MX6Q_EXAMPLES),y)
 define GPU_VIV_BIN_MX6Q_INSTALL_EXAMPLES
 	mkdir -p $(TARGET_DIR)/usr/share/examples/
-	cp -r $(@D)-beta-hfp/opt/* $(TARGET_DIR)/usr/share/examples/
+	cp -r $(@D)-hfp/opt/* $(TARGET_DIR)/usr/share/examples/
 endef
 endif
 
@@ -82,7 +82,7 @@ endif
 # to the wrong library
 define GPU_VIV_BIN_MX6Q_INSTALL_TARGET_CMDS
 	$(GPU_VIV_BIN_MX6Q_INSTALL_EXAMPLES)
-	cp -a $(@D)-beta-hfp/usr/lib $(TARGET_DIR)/usr
+	cp -a $(@D)-hfp/usr/lib $(TARGET_DIR)/usr
 	for lib in EGL GAL VIVANTE; do \
 		for f in $(TARGET_DIR)/usr/lib/lib$${lib}-*.so; do \
 			case $$f in \
