f543fd319029912fc7a516e705a4414881e7790c
liyaoshi <liyaoshi@gmail.com>
2014-06-13 16:13:54 +0800

    update freescale package from fsl site

--- package/freescale-imx/freescale-imx.mk
+++ package/freescale-imx/freescale-imx.mk
@@ -7,6 +7,6 @@
 FREESCALE_IMX_VERSION = 3.10.17-1.0.0
 
 # No official download site from freescale, just this mirror
-FREESCALE_IMX_MIRROR_SITE   = http://download.ossystems.com.br/bsp/freescale/source
+FREESCALE_IMX_MIRROR_SITE   = http://www.freescale.com/lgfiles/NMG/MAD/YOCTO
 
 include $(sort $(wildcard package/freescale-imx/*/*.mk))
