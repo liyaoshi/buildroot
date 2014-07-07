b3817abb1aec88ad65dbc27538390c3c89379125
liyaoshi <liyaoshi@gmail.com>
2014-07-07 09:49:15 +0800

    update kobs ng to 3.10.17

--- package/kobs-ng/kobs-ng.mk
+++ package/kobs-ng/kobs-ng.mk
@@ -5,8 +5,9 @@
 ################################################################################
 
 # kobs-ng versions have never made much sense :(
-KOBS_NG_VERSION = 3.0.35-4.0.0
-KOBS_NG_SITE = http://repository.timesys.com/buildsources/k/kobs-ng/kobs-ng-$(KOBS_NG_VERSION)/
+KOBS_NG_VERSION = 3.10.17-1.0.0
+KOBS_NG_SITE = http://www.freescale.com/lgfiles/NMG/MAD/YOCTO
+KOBS_NG_SOURCE = imx-kobs-3.10.17-1.0.0.tar.gz
 KOBS_NG_LICENSE = GPLv2+
 KOBS_NG_LICENSE_FILES = COPYING
 
