a5c1bdc121872959958b54c5986158cbfa7e45e2
liyaoshi <liyaoshi@gmail.com>
2014-07-09 09:14:47 +0800

    update libfslvpuwrap to 1.0.45

--- package/libfslvpuwrap/libfslvpuwrap.mk
+++ package/libfslvpuwrap/libfslvpuwrap.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBFSLVPUWRAP_VERSION = 1.0.17
-# No official download site from freescale, just this mirror
-LIBFSLVPUWRAP_SITE = http://download.ossystems.com.br/bsp/freescale/source
+LIBFSLVPUWRAP_VERSION = 1.0.45
+LIBFSLVPUWRAP_SITE =  http://www.freescale.com/lgfiles/NMG/MAD/YOCTO
+LIBFSLVPUWRAP_SOURCE = libfslvpuwrap-1.0.45.bin
 LIBFSLVPUWRAP_LICENSE = Freescale Semiconductor Software License Agreement
 LIBFSLVPUWRAP_LICENSE_FILES = EULA.txt
 LIBFSLVPUWRAP_REDISTRIBUTE = NO
@@ -14,5 +14,12 @@ LIBFSLVPUWRAP_REDISTRIBUTE = NO
 LIBFSLVPUWRAP_INSTALL_STAGING = YES
 
 LIBFSLVPUWRAP_DEPENDENCIES += imx-lib
+
+
+define LIBFSLVPUWRAP_EXTRACT_CMDS
+        (cd $(BUILD_DIR); \
+		             sh $(DL_DIR)/$(LIBFSLVPUWRAP_SOURCE) --force --auto-accept)
+
+endef
 
 $(eval $(autotools-package))
