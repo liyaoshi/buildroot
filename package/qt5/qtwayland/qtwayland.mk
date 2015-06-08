88a2ed23c64d41d894b9e66dbab61092178ac4a7
HuangXiongwei <HuangXiongwei@saicmotor.com>
2015-06-08 11:38:35 +0800

    add qtwayland

---
+++ package/qt5/qtwayland/qtwayland.mk
@@ -0,0 +1,37 @@
+QTWAYLAND_VERSION = 182488129c3f6a67a7e781fdb7c0147777191991
+QTWAYLAND_SITE = git://gitorious.org/qt/qtwayland.git
+QTWAYLAND_SITE_METHOD = git
+
+QTWAYLAND_DEPENDENCIES = wayland weston
+
+QTWAYLAND_LICENSE = GPLv2
+QTWAYLAND_LICENSE_FILES = COPYING
+
+
+define QTWAYLAND_CONFIGURE_CMDS
+        (cd $(@D); \
+       mkdir -p  $(@D)/include/QtWaylandClient/private/ ;\
+       cd  include/QtWaylandClient/private/ ;\
+       ln -s ../../../src/client/hardwareintegration/*.h . ; \
+       ln -s ../../../src/client/*.h . ;\
+       cd $(@D);       \
+	PATH=$(HOST_DIR)/usr/bin:$(PATH) $(HOST_DIR)/usr/bin/qmake \
+	)
+endef
+
+define QTWAYLAND_BUILD_CMDS
+        $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define QTWAYLAND_INSTALL_STAGING_CMDS
+        $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install
+        $(QT5_LA_PRL_FILES_FIXUP) 
+endef
+
+define QTWAYLAND_INSTALL_TARGET_CMDS
+	cd $(@D); \
+	cp -arf lib/libQt5WaylandClient.so* $(TARGET_DIR)/usr/lib/ ;\
+	cp -dpfr plugins/platforms/libqwayland-egl.so $(TARGET_DIR)/usr/lib/qt/plugins/platforms
+endef
+
+$(eval $(generic-package))
