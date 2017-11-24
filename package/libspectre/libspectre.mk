a4c3ed87eabc8ccb8b1c95cba187e97a3eb63e57
HuangXiongwei <Xiongwei.huang@visteon.com>
2017-11-24 18:26:48 +0800

    libspectre use 0.2.7

--- package/libspectre/libspectre.mk
+++ package/libspectre/libspectre.mk
@@ -4,17 +4,13 @@
 #
 ################################################################################
 
-# This correpsonds to SDK 02.00.00.00
-LIBSPECTRE_VERSION = f88d2c259242936bbc3c74bf43107626c0f5ad61
-LIBSPECTRE_SITE =  git://anongit.freedesktop.org/git/libspectre 
+LIBSPECTRE_VERSION = 0.2.7
+LIBSPECTRE_SITE =  https://libspectre.freedesktop.org/releases/
+LIBSPECTRE_SOURCE =  libspectre-$(LIBSPECTRE_VERSION).tar.gz
 LIBSPECTRE_LICENSE = GPL-2.0
 LIBSPECTRE_LICENSE_FILES = GPL-COPYING 
 
 LIBSPECTRE_INSTALL_TARGET = YES
 LIBSPECTRE_INSTALL_STAGING = YES
 
-define LIBSPECTRE_RUN_AUTOGEN
-        cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
-endef
-LIBSPECTRE_PRE_CONFIGURE_HOOKS += LIBSPECTRE_RUN_AUTOGEN
 $(eval $(autotools-package))
