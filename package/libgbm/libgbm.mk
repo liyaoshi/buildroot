a37400cf4a1af048be895d1bff588bffcdc4d19d
HuangXiongwei <Xiongwei.huang@visteon.com>
2017-11-29 03:38:04 -0500

    fix build libgbm with -O0

--- package/libgbm/libgbm.mk
+++ package/libgbm/libgbm.mk
@@ -14,7 +14,13 @@ LIBGBM_LICENSE_FILES = LICENSE
 LIBGBM_DEPENDENCIES = libdrm udev ti-sgx-um
 
 define LIBGBM_RUN_AUTOGEN
-	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh 
 endef
+
+define LIBGBM_RUN_FIXMAKE
+	$(SED) 's/\-O3/\-O0/' $(@D)/Makefile
+endef
+
+LIBGBM_PRE_BUILD_HOOKS += LIBGBM_RUN_FIXMAKE
 LIBGBM_PRE_CONFIGURE_HOOKS += LIBGBM_RUN_AUTOGEN
 $(eval $(autotools-package))
