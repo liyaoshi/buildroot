b4bb1357b5cd6bf67323e67758f5a95882b22191
HuangXiongwei <Xiongwei.huang@visteon.com>
2018-07-30 23:38:21 -0400

    update cmem build

--- package/cmem/cmem.mk
+++ package/cmem/cmem.mk
@@ -12,5 +12,17 @@ CMEM_INSTALL_STAGING = YES
 
 CMEM_DEPENDENCIES = linux
 
+define CMEM_BUILD_KO
+     cd $(@D) ; make -f lu.mak TOOLCHAIN_PREFIX=$(TARGET_CROSS) KERNEL_INSTALL_DIR=$(LINUX_DIR) module
+endef
+
+CMEM_POST_BUILD_HOOKS += CMEM_BUILD_KO
+
+define CMEM_INSTALL_KO
+     cp -arvf $(@D)/src/cmem/module/cmemk.ko $(TARGET_DIR)/lib/modules
+endef
+
+CMEM_POST_INSTALL_TARGET_HOOKS += CMEM_INSTALL_KO
+
 $(eval $(autotools-package))
 
