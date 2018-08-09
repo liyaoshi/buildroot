5727f4ff9b66d50a3d947ef34bdf81d288749755
HuangXiongwei <Xiongwei.huang@visteon.com>
2018-08-09 03:51:40 -0400

    add ti-gc320-test

--- package/ti-gc320-lib/ti-gc320-lib.mk
+++ package/ti-gc320-lib/ti-gc320-lib.mk
@@ -5,22 +5,21 @@
 ################################################################################
 
 # This correpsonds to SDK 02.00.00.00
-TI_GC320_LIB_VERSION = ab0ca5bff345f6c13807fea3c4acabf5f2b9b10a
-TI_GC320_LIB_SITE =  git://git.ti.com/graphics/ti-gc320-libs.git
-TI_GC320_LIB_LICENSE = TI TSPA License
+TI_GC320_LIB_VERSION = c0afab259de59909cfe74c01f3f7fbaa147f94b5
+TI_GC320_LIB_SITE = git://git.ti.com/graphics/ti-gc320-libs.git
+TI_GC320_LIB_LICENSE = GPL
 TI_GC320_LIB_INSTALL_STAGING = YES
 
 # ti-gc320-lib is a egl/gles provider only if libdrm is installed
 TI_GC320_LIB_DEPENDENCIES = libdrm wayland libgbm ti-sgx-km
 
-
 define TI_GC320_LIB_INSTALL_TARGET_CMDS
-
 	cp -arf $(@D)/targetfs/jacinto6evm/lib/* $(TARGET_DIR)/usr/lib
-
 endef
 
-
-
+define TI_GC320_LIB_INSTALL_STAGING_CMDS
+	cp -arf $(@D)/targetfs/jacinto6evm/lib/* $(STAGING_DIR)/usr/lib
+	cp -arf $(@D)/targetfs/jacinto6evm/include/* $(STAGING_DIR)/usr/include
+endef
 
 $(eval $(generic-package))
--- package/ti-gc320-test/ti-gc320-test.mk
+++ package/ti-gc320-test/ti-gc320-test.mk
@@ -5,22 +5,14 @@
 ################################################################################
 
 # This correpsonds to SDK 02.00.00.00
-TI_GC320_LIB_VERSION = ab0ca5bff345f6c13807fea3c4acabf5f2b9b10a
-TI_GC320_LIB_SITE =   git://git.ti.com/graphics/ti-gc320-test.git
-TI_GC320_LIB_LICENSE = TI TSPA License
-TI_GC320_LIB_INSTALL_STAGING = YES
+TI_GC320_TEST_VERSION = 53207f8e360a3336276b62066a1d8a9e923de17e
+TI_GC320_TEST_SITE =   git://git.ti.com/graphics/ti-gc320-test.git
+TI_GC320_TEST_LICENSE = TI TSPA License
 
 # ti-gc320-lib is a egl/gles provider only if libdrm is installed
-TI_GC320_LIB_DEPENDENCIES = libdrm wayland libgbm ti-gc320-lib
+TI_GC320_TEST_DEPENDENCIES = ti-gc320-lib
 
-
-define TI_GC320_LIB_INSTALL_TARGET_CMDS
-
+define TI_GC320_TEST_INSTALL_TARGET_CMDS
 	cp -arf $(@D)/targetfs/jacinto6evm/lib/* $(TARGET_DIR)/usr/lib
-
 endef
-
-
-
-
 $(eval $(generic-package))
