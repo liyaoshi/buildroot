621119e672926aee406d19bcd5010c5a4eedcf78
HuangXiongwei <Xiongwei.huang@visteon.com>
2018-08-16 02:23:07 -0400

    update gc320 test build command

--- package/ti-gc320-test/ti-gc320-test.mk
+++ package/ti-gc320-test/ti-gc320-test.mk
@@ -1,7 +1,8 @@
 ################################################################################
 #
 # ti-gc320-test
-#
+# make -f makefile.linux ARCH_TYPE=armv7-a CPU_TYPE=cortex-a15  VIVANTE_SDK_INC=/home/xhuang10/github/ti-gc320-test/sdk/include VIVANTE_SDK_LIB=/home/xhuang10/github/ti-gc320-lib/targetfs/jacinto6evm/lib EGL_API_FB=1 SDK_DIR=/home/xhuang10/github/ti-gc320-test/sdk VIVANTE_SDK_DIR=/home/xhuang10/github/ti-gc320-test/sdk OBJ_DIR=/home/xhuang10/github/ti-gc320-test/sdk/samples/hal/unit_test
+
 ################################################################################
 
 # This correpsonds to SDK 02.00.00.00
@@ -12,7 +13,8 @@ TI_GC320_TEST_LICENSE = TI TSPA License
 # ti-gc320-lib is a egl/gles provider only if libdrm is installed
 TI_GC320_TEST_DEPENDENCIES = ti-gc320-lib
 
-define TI_GC320_TEST_INSTALL_TARGET_CMDS
-	cp -arf $(@D)/targetfs/jacinto6evm/lib/* $(TARGET_DIR)/usr/lib
+define TI_GC320_TEST_BUILD_CMDS
+	cd $(@D)/tests/src;  AQROOT=$(@D)/tests/src make -f makefile.linux CROSS_COMPILE=$(TARGET_CROSS) ARCH_TYPE=armv7-a CPU_TYPE=cortex-a9 VIVANTE_SDK_DIR=$(@D)/sdk VIVANTE_SDK_INC=$(@D)/sdk/include VIVANTE_SDK_LIB=$(STAGING_DIR)/usr/lib DESTDIR=$(STAGING_DIR) SDK_DIR=$(@D)/tests/ EGL_API_FB=1 AQROOT=$(@D)/tests/src OBJ_DIR=$(@D)/sdk/samples/hal/unit_test
+
 endef
 $(eval $(generic-package))
